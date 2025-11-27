e8910017358eb046a3b1c84fc6bba115b737a674
LeKrieger <157001759+lekrieger@users.noreply.github.com>
2025-11-27 11:05:12 +0000

    main

--- PV/lekrieger-dwerwein_Pac-Man/src/Paku_Paku.java
+++ PV/lekrieger-dwerwein_Pac-Man/src/Paku_Paku.java
@@ -1,16 +1,35 @@
+import java.io.IOException;
+
 import javax.swing.*;
 
 public class Paku_Paku {
+
+    
     public static void main(String[] args) throws Exception {
         
-        Grid<GameObject> loadedGrid = LevelLoader.loadLevel("src/Levels/level1.txt"); //Grid aus Datei laden
-        GamePanel gamePanel  = new GamePanel(loadedGrid);  // Neues GamePanel mit geladenem Grid erstellen
-        JFrame frame = new JFrame("Paku Paku"); // JFrame erstellen
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(gamePanel);   // Neues GamePanel zum JFrame hinzufügen
-        frame.pack();   // JFrame auf bevorzugte Größe setzen
-        frame.repaint();    // JFrame neu zeichnen
-        frame.setVisible(true); // JFrame sichtbar machen
+        System.out.println("Ich suche in: " + new java.io.File(".").getAbsolutePath());
+        // JFrame vorbereiten
+        JFrame window = new JFrame("Paku Paku");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false); // Feste Größe 
+
+        try {
+            // Level laden
+            Grid<GameObject> grid = LevelLoader.loadLevel("PV/lekrieger-dwerwein_Pac-Man/Levels/level1.txt");
+
+            // Panel erstellen und Grid übergeben
+            GamePanel panel = new GamePanel(grid);
+
+            window.add(panel);
+            window.pack(); // Fenstergröße passt sich automatisch an das Panel (Grid * 20px) an
+            window.setLocationRelativeTo(null); // zentriert das Fenster
+            window.setVisible(true);
+
+        } catch (IOException e) {
+            // Fehlerbehandlung
+            System.err.println("FEHLER: Konnte Level-Datei nicht laden!");
+            e.printStackTrace();
+        }
     }
 }
 
