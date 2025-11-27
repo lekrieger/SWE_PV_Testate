5eb98393ce9ee8f446224ecbbec44b8924e19ef7
DeWerwein <dennis.werwein@gmail.com>
2025-11-27 11:47:47 +0100

    push

--- PV/lekrieger-dwerwein_Pac-Man/src/Paku_Paku.java
+++ PV/lekrieger-dwerwein_Pac-Man/src/Paku_Paku.java
@@ -1,22 +1,16 @@
 import javax.swing.*;
 
-//Test 
-
 public class Paku_Paku {
     public static void main(String[] args) throws Exception {
         
-        Grid<GameObject> grid = new Grid<>(20, 20); // Grid mit 20x20 Zellen erstellen
-        GamePanel gamePanel = new GamePanel(grid); // GamePanel mit dem Grid erstellen
+        Grid<GameObject> loadedGrid = LevelLoader.loadLevel("src/Levels/level1.txt"); //Grid aus Datei laden
+        GamePanel gamePanel  = new GamePanel(loadedGrid);  // Neues GamePanel mit geladenem Grid erstellen
         JFrame frame = new JFrame("Paku Paku"); // JFrame erstellen
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(gamePanel); // GamePanel zum JFrame hinzufügen
-        frame.pack(); // JFrame auf bevorzugte Größe setzen
+        frame.add(gamePanel);   // Neues GamePanel zum JFrame hinzufügen
+        frame.pack();   // JFrame auf bevorzugte Größe setzen
+        frame.repaint();    // JFrame neu zeichnen
         frame.setVisible(true); // JFrame sichtbar machen
-
-        Grid<GameObject> loadedGrid = LevelLoader.loadLevel("level1.txt");
-        gamePanel = new GamePanel(loadedGrid);
-        frame.add(gamePanel);
-        frame.pack();
-        frame.repaint();
     }
 }
+
