978aecb593a91951d49038f569cef29bbdd6266e
LeKrieger <157001759+lekrieger@users.noreply.github.com>
2025-11-27 10:36:22 +0000

    fixes

--- PV/lekrieger-dwerwein_Pac-Man/src/GamePanel.java
+++ PV/lekrieger-dwerwein_Pac-Man/src/GamePanel.java
@@ -4,10 +4,11 @@ import java.awt.Dimension;
 public class GamePanel extends JPanel {
     
     private Grid<GameObject> grid; // Variable anlegen für das Grid
+    public static final int scale = 20; // Skalierungsfaktor für die Größe der Zellen
 
     public GamePanel(Grid<GameObject> grid) {
         this.grid = grid;
-        setPreferredSize(new Dimension(grid.getCols() * 20, grid.getRows() * 20));
+        setPreferredSize(new Dimension(grid.getCols() * scale, grid.getRows() * scale)); // Größe des Panels basierend auf Grid-Größe (Skalierung der Levelgröße * scale Pixel pro Zelle)
     }
 
     @Override
